86f24aff0b32b6526befface660cb106e0ad9679
Imre Mehesz <imehesz@imehesz-laptop.(none)>
2010-10-26 22:09:33 -0400

    added advanced security for the ADMIN user, aka me ... hehe

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -24,7 +24,7 @@ class UserIdentity extends CUserIdentity
 		);
 		if(!isset($users[$this->username]))
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		else if($users[$this->username]!==$this->password)
+		else if( md5($this->password) != "b5b1ad28d110b7da358cd74fb3c4ce5a" )
 			$this->errorCode=self::ERROR_PASSWORD_INVALID;
 		else
 			$this->errorCode=self::ERROR_NONE;
