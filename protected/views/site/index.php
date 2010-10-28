7279dcc3ede89401b36bd79af8c8922c7c0aa2e2
Imre Mehesz <imehesz@imehesz-laptop.(none)>
2010-10-27 22:56:21 -0400

    created the front page and started the features, removed some menu items ... getting there :)

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,16 +1,55 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<h1>Welcome to the <i><?php echo CHtml::encode(Yii::app()->name); ?></i> site.</h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<div class="front-top-block">
+	<h3 class="text-center">Hello</h3>
+	<p>This site was developed to track the never ending <strong>ping-pong</strong> war between <a href="http://tampadigital.com" target="_blank" title="Tampa Digiatal - Let's Think Together">Tampa Digiatal</a> employees. Everything else is pretty self explanitaritory :)</p>
+	<p>
+		If you are en employee, just get a <a href="http://en.wikipedia.org/wiki/Table_tennis_racket" target="_blank">paddle</a> and <strong>join the fun</strong>!
+	</p>
+	<p>Thanks for checking us out!</p>
+</div>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <tt><?php echo __FILE__; ?></tt></li>
-	<li>Layout file: <tt><?php echo $this->getLayoutFile('main'); ?></tt></li>
-</ul>
+<div class="front-top-block">
+	<h3 class="text-center">Photos</h3>
+	<?php echo CHtml::link( CHtml::image( 'http://www.goldbamboo.com/images/content/9488-400px-tt-table-table-tennis.gif' ), 'http://en.wikipedia.org/wiki/Table_tennis', array( 'title' => 'Daddy, what is Table Tennis?', 'target' => '_blank' ) ); ?>
+</div>
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<div style="clear:both;"></div>
+
+<div class="front-block">
+	<h3 class="text-center">Stats</h3>
+	<div>
+		<div style="width:150px;">
+			<table>
+				<thead>
+					<tr>
+						<th colspan="3" align="right">Top 10</th>
+					</tr>
+					<tr>
+						<th>Pos.</th>
+						<th>Name</th>
+						<th>Win</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php $top10 = Player::model()->findAll( 'created <> 0 ORDER BY won DESC LIMIT 10' ); ?>
+					<?php $pos=1; foreach( $top10 as $player ) : ?>
+						<tr>
+							<td><?php echo $pos > 3 ? $pos : '<span style="font-weight:bolder;">' . $pos  . '</span>'; ?>. </td>
+							<td><?php echo $pos > 3 ? $player->name : "<span style='font-weight:bolder;'>{$player->name}</span>"; ?></td>
+							<td><?php echo $player->won > 0 ? $player->won . ' (' . round(($player->won / ($player->won+$player->lost))*100 ) . '%)' : $player->won; ?></td>
+						</tr>
+					<?php $pos++; endforeach; ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+<div class="front-block">
+	<h3 class="text-center">Coming Soon</h3>
+	<img src="http://www.djelectricnoiz.net/images/under_construction.png" />
+</div>
+
