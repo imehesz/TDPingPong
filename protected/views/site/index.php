05d2fa6512ef4e968aa0fc3b6d307467762e8be7
Imre Mehesz <imremehesz@WKST-TDS16-LX.(none)>
2010-11-11 13:51:08 -0500

    ok, I fixed this once already!?

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -42,7 +42,7 @@ $(document).ready(function(){
 
 <div class="front-top-block">
 	<h3 class="text-center">Hello</h3>
-	<p>This site was developed to track the never ending <strong>ping-pong</strong> war between <a href="http://tampadigital.com" target="_blank" title="Tampa Digital - Let's Think Together">Tampa Digiatal</a> employees. Everything else is pretty self explanatory :)</p>
+	<p>This site was developed to track the never ending <strong>ping-pong</strong> war between <a href="http://tampadigital.com" target="_blank" title="Tampa Digital - Let's Think Together">Tampa Digital</a> employees. Everything else is pretty self explanatory :)</p>
 	<p>
 		If you are en employee, just get a <a href="http://en.wikipedia.org/wiki/Table_tennis_racket" target="_blank">paddle</a> and <strong>join the fun</strong>! 
 	</p>
